b22357e6ca48b9c5581a795fde5a32b05155cf62
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-12-14 15:07:12 +0800

    Implemented product progress percentage

--- Thread Safe/threadsafe.py
+++ Thread Safe/threadsafe.py
@@ -46,3 +46,31 @@ class Product:
 
     def __str__(self):
         return self.label
+
+class Priority(IntEnum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
+PRIORITIZED_PRODUCTS = (
+    Product(Priority.HIGH, ":1st_place_medal:"),
+    Product(Priority.MEDIUM, ":2nd_place_medal:"),
+    Product(Priority.LOW,":3rd_place_medal:"),
+)
+
+class Worker(threading.Thread):
+    def __init__(self, speed, buffer):
+        super().__init__(daemon=True)
+        self.speed = speed
+        self.buffer = buffer
+        self.product = None
+        self.working = False
+        self.progress = 0
+
+    @property
+    def state(self):
+        if self.working:
+            return f"{self.product} ({self.progress}%)"
+        return ":zzz: Idle"
+
+    
