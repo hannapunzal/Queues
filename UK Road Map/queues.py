fae5ff32aeb7a36624eef88b84f6eb7d391d0033
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-12-14 21:41:04 +0800

    Created iteration function with loop for dequeuing

--- UK Road Map/queues.py
+++ UK Road Map/queues.py
@@ -7,4 +7,8 @@ from typing import AnyStr
 class IterableMixin:
     def __len__(self):
         return len(self._elements)
-        
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+            
