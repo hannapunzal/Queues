63d2bd0726e701d71c66731f0739c6f41eabaa82
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-12-14 21:39:09 +0800

    Added iterable mixin class

---
+++ UK Road Map/queues.py
@@ -0,0 +1,8 @@
+from collections import deque
+from dataclasses import dataclass 
+from heapq import heapify, heappop, heappush
+from itertools import count
+from typing import AnyStr
+
+class IterableMixin:
+    
