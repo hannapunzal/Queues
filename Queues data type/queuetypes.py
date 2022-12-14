d00e693abdaa714ee1a5e3681cf6140efdce466f
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-12-14 14:13:44 +0800

    Priority Queue data type

--- Queues data type/queuetypes.py
+++ Queues data type/queuetypes.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass
 from itertools import count
 
 #QUEUES
-
 class Iterable:
     def __len__(self):
         return len(self._elements)
@@ -22,7 +21,21 @@ class Queue(Iterable):
 
     def dequeue(self):
         return self._elements.popleft()
+
 #STACKS
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
+    
+#PRIO
+class PriorityQueue(Iterable):
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter),value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements) [-1]
