0f85d87dd9fb26f1a6f0796064fb83cfa0b26eab
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-12-14 21:45:41 +0800

    Implemented dequeuing for stack class

--- UK Road Map/queues.py
+++ UK Road Map/queues.py
@@ -21,3 +21,7 @@ class Queue(IterableMixin):
     
     def dequeue(self):
         return self._elements.popleft()
+
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
