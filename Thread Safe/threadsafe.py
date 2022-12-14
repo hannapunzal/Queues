fd7984da219c2360f3f49e8fe42370bc45586323
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-12-14 15:22:34 +0800

    Added while loop for the producer

--- Thread Safe/threadsafe.py
+++ Thread Safe/threadsafe.py
@@ -94,3 +94,9 @@ class Producer(Worker):
         self.products = products
 
     def run(self):
+        while True:
+            self.product = self.buffer.get()
+            self.simulate_work()
+            self.buffer.task_done()
+            self.simulate_idle()
+            
