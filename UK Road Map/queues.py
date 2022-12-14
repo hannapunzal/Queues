05eb126536d797bcd9157e7f2a6ff2180ba88593
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-12-14 21:53:48 +0800

    Made Mutable class with initialization and setting items

--- UK Road Map/queues.py
+++ UK Road Map/queues.py
@@ -43,3 +43,20 @@ class Element:
     priority: float
     count: int
     value: Any 
+
+class MutableMinHeap(IterableMixin):
+    def __init__(self):
+        super().__init()
+        self._elements_by_value = []
+        self._elements = []
+        self._counter = count()
+
+    def __setitem__(self, unique_value, priority):
+        if unique_value in self._elements_by_value:
+            self._elements_by_value[unique_value].priority = priority
+            heapify(self._elements)
+
+        else:
+            element = Element(priority, next(self._counter), unique_value)
+            self._elements_by_value[unique_value] = element
+            heappush(self._elements, element)
