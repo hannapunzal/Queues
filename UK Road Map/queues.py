40db0862343bcc28a8c719f73a02b0a59f3cc47c
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-12-14 21:48:47 +0800

    Established functions for the queuing and dequeuing with priority

--- UK Road Map/queues.py
+++ UK Road Map/queues.py
@@ -30,3 +30,11 @@ class PriorityQueue(IterableMixin):
     def __init__(self):
         self._elements = []
         self._counter = count()
+    
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
+        
